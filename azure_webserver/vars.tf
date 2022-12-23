e2879c79c826d580f490a766bb8c685ecd356a3f
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2022-12-23 14:20:07 +0530

    Update vars.tf

--- azure_webserver/vars.tf
+++ azure_webserver/vars.tf
@@ -7,3 +7,15 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   default = "East US"
 }
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Variable declaration to read from TF_VAR_ARM_SUBSCRIPTION_ID."
+}
+
+variable "ARM_CLIENT_ID" {
+  description = "Variable declaration to read from TF_VAR_ARM_CLIENT_ID."
+}
+
+variable "ARM_TENANT_ID" {
+  description = "Variable declaration to read from TF_VAR_ARM_TENANT_ID."
+}
