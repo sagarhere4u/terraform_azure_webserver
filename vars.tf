d46748b75862d8061ba1e834a324aacaac41e6e3
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-01-15 14:35:51 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -7,3 +7,12 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   default = "East US"
 }
+
+variable "subscription_id" {
+}
+
+variable "client_id" {
+}
+
+variable "tenant_id" {
+}
