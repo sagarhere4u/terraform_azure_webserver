18cf926331baefc750473becf148b4fbb3c603e9
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-01-15 14:14:49 +0530

    Delete aws_webserver directory

--- aws_webserver/out.tf
+++
@@ -1,8 +0,0 @@
-output "public_ip" {
-  value = aws_instance.webserver.public_ip
-}
-
-output "url" {
-  value = "http://${aws_instance.webserver.public_dns}:80"
-}
-
--- aws_webserver/web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install apache2 -y
-sudo systemctl restart apache2
-sudo systemctl enable apache2
