2929a1db59e5316874ff0c51bc1e871114f73079
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-01-15 14:22:58 +0530

    Delete azure_webserver directory

--- azure_webserver/apache2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update && sudo apt -y install apache2
-
-
-echo '<!doctype html><html><body><h1>Hello if you see this than you have apache running!</h1></body></html>' | sudo tee /var/www/html/index.html
--- azure_webserver/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
-  default = "webserver"
-}
-
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-  default = "East US"
-}
